cee954ed718485e01643feba0e00dc9229fa7344
zurda <m.weizman@gmail.com>
2020-01-10 19:59:32 +0000

    Deploying to gh-pages from  9c83087b0e2830c8908e8b1edaba57d810a92974

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangman-react/precache-manifest.7c7ed556697e444ce70c819e71eef42e.js"
+  "/hangman-react/precache-manifest.243fc79923cc2bb60a7c33642e0b70fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangman-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
