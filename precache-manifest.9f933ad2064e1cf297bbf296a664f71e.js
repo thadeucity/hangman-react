d096c6f0a7f4e8576e6e9dec939d92c9e650cd2b
zurda <m.weizman@gmail.com>
2020-08-23 18:39:08 +0000

    Deploying to gh-pages from  @ 64964f85e22e201106b3bdf820187e61277455cd 🚀

---
+++ precache-manifest.9f933ad2064e1cf297bbf296a664f71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf5057bec1f4efc05a4046b808140be",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "fbd86cb4597eb99040ae",
+    "url": "/hangman-react/static/js/2.558d90f0.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/hangman-react/static/js/2.558d90f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f316a5147384a5c9aafa",
+    "url": "/hangman-react/static/js/main.d9fd95a8.chunk.js"
+  },
+  {
+    "revision": "734e1bf92ff7cac101a8",
+    "url": "/hangman-react/static/js/runtime-main.e1871ce5.js"
+  },
+  {
+    "revision": "aa86276afbc0abfc19c5fb05e9fb4c91",
+    "url": "/hangman-react/static/media/down_arrow.aa86276a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangman-react/precache-manifest.c93f9558029f3d99ae5380177e218929.js"
+  "/hangman-react/precache-manifest.9f933ad2064e1cf297bbf296a664f71e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangman-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
